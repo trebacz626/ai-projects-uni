94c76be5b0abba20d5aff43f1e0d49acb078ae86
Kacper Trebacz <trebacz.kacper@gmail.com>
2020-09-13 21:22:05 +0200

    better producer consumer

--- Operation Systems and Concurrent Programming/final/consumer.c
+++ Operation Systems and Concurrent Programming/final/consumer.c
@@ -17,13 +17,15 @@ int main(int argc, char *argv[])
     sem_consumer_counter = sem_open("/consumer_counter", 0, 0600, 0);
     sem_producer_counter = sem_open("/producer_counter", 0, 0600, 0);
     sem_consumer_sync = sem_open("/consumer_sync", 0, 0600, 0);
-    int fd = shm_open("/memory", O_CREAT | O_RDWR, 0600);
+    int fd = shm_open("/memory", O_RDWR, 0600);
+    if(sem_consumer_counter == SEM_FAILED || sem_producer_counter == SEM_FAILED || sem_consumer_sync == SEM_FAILED || fd == -1){
+        printf("Could not access means of synchronisations, please run start first \n");
+        return 1;
+    }
     int *addr = mmap(NULL, (N+2)*sizeof(int), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
 
     while (1)
     {
-        //pretend that you are doing sth 
-        sleep(rand() % 5);
 
         sem_wait(sem_consumer_counter);
         sem_wait(sem_consumer_sync);
@@ -32,6 +34,8 @@ int main(int argc, char *argv[])
         printf("Consuming %d\n", addr[addr[N+1]]);
         // end consume from buffer
         sem_post(sem_consumer_sync);
+        //pretend that you are doing sth 
+        sleep(rand() % 5);
         sem_post(sem_producer_counter);
     }
 }
--- Operation Systems and Concurrent Programming/final/producer.c
+++ Operation Systems and Concurrent Programming/final/producer.c
@@ -16,21 +16,22 @@ int main(int argc, char *argv[])
     sem_consumer_counter = sem_open("/consumer_counter", 0, 0600, 0);
     sem_producer_counter = sem_open("/producer_counter", 0, 0600, 0);
     sem_producer_sync = sem_open("/producer_sync", 0, 0600, 0);
-    int fd = shm_open("/memory", O_CREAT | O_RDWR, 0600);
+    int fd = shm_open("/memory", O_RDWR, 0600);
+    if(sem_consumer_counter == SEM_FAILED || sem_producer_counter == SEM_FAILED || sem_producer_sync == SEM_FAILED || fd == -1){
+        printf("Could not access means of synchronisations, please run start first \n");
+        return 1;
+    }
     int *addr = mmap(NULL, (N+2)*sizeof(int), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
     srand(time(0));
     int product_type = atoi(argv[1]);
     // printf("I produce %d\n", product_type);
     while (1)
     {  
+        sem_wait(sem_producer_counter);
         //pretend that you are doing sth
         sleep(rand() % 5); 
-        sem_wait(sem_producer_counter);
         sem_wait(sem_producer_sync);
-        //produce to buffer
-        struct timeval start;
-        // gettimeofday(&start, NULL);
-        // printf("time %ld.%ld\n", start.tv_sec,start.tv_usec);
+        //move to buffer
         addr[N+2] = (addr[N+2]+1) % N;
         addr[addr[N+2]] = product_type;
         printf("produced %d\n", product_type);
